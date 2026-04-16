db20026f0b03eb709cd28784ff6c6282ae2b0dff
Aditi <aditihubli21@gmail.com>
2026-04-16 17:37:49 +0530

    Phase1:Object storage

--- object.c
+++ object.c
@@ -7,8 +7,6 @@
 //
 // PROVIDED functions: compute_hash, object_path, object_exists, hash_to_hex, hex_to_hash
 // TODO functions:     object_write, object_read
-
-
 #include "pes.h"
 #include <stdio.h>
 #include <stdlib.h>
