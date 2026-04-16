fc0d13ad8c42f689236f690a56e39c0f1e5cbb69
Aditi <aditihubli21@gmail.com>
2026-04-16 18:03:19 +0530

    Phase 4: Add HEAD update and parent linking

--- commit.c
+++ commit.c
@@ -24,10 +24,6 @@
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
-
-
-
-
 // Forward declarations (implemented in object.c)
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out);
