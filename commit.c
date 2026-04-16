9e0a1864b17cc7ab9cdb13321543a1db574bf9b1
Aditi <aditihubli21@gmail.com>
2026-04-16 18:04:11 +0530

    Phase 4: Finalize commit workflow and testing

--- commit.c
+++ commit.c
@@ -14,6 +14,8 @@
 // PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
 // TODO functions:     commit_create
 
+
+
 #include "commit.h"
 #include "index.h"
 #include "tree.h"
